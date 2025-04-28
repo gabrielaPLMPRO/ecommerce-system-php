3a3fb744adb6ebff63fa2c1e8f9d1bebe4e84fbb
Francisco Andrade <franciscocembrani@gmail.com>
2025-04-28 15:38:27 -0300

    mudanças script

--- database/db_schema.sql
+++ database/db_schema.sql
@@ -1,6 +1,21 @@
 CREATE DATABASE virtual_store;
 \c virtual_store;
 
+CREATE TABLE IF NOT EXISTS endereco
+(
+    id SERIAL PRIMARY KEY,
+    rua VARCHAR(20),
+    numero VARCHAR(10),
+    complemento VARCHAR(100),
+    bairro VARCHAR(50),
+    cep varchar(10),
+    cidade VARCHAR(50),
+    estado VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
 CREATE TABLE IF NOT EXISTS usuarios (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
@@ -14,40 +29,70 @@ CREATE TABLE IF NOT EXISTS usuarios (
 CREATE TABLE IF NOT EXISTS fornecedores (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    contato VARCHAR(255) NOT NULL,
+    descricao VARCHAR(1000),
+	telefone VARCHAR(20),
+	email VARCHAR(50),
+    endereco_id INT NOT NULL REFERENCES endereco(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+   
 
 CREATE TABLE IF NOT EXISTS produtos (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     descricao TEXT NOT NULL,
-    preco DECIMAL(10, 2) NOT NULL,
-    estoque INT NOT NULL DEFAULT 0,
+    foto VARCHAR(4000), -- guid
     fornecedor_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_fornecedor FOREIGN KEY (fornecedor_id) REFERENCES fornecedores(id) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS estoque 
+(
+	id SERIAL PRIMARY KEY,
+    preco DECIMAL(10, 2) NOT NULL,
+    estoque INT NOT NULL DEFAULT 0,
+	produto_id INT NOT NULL REFERENCES produtos(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS clientes 
+(
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(200),
+	telefone VARCHAR(20),
+	email VARCHAR(50),
+	cartao_credito VARCHAR(50),
+    endereco_id INT NOT NULL REFERENCES endereco(id),
+    usuario_id INT NOT NULL REFERENCES usuarios(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS pedidos (
     id SERIAL PRIMARY KEY,
+    cliente_id INT NOT NULL REFERENCES clientes(id),
     usuario_id INT NOT NULL,
     status VARCHAR(10) CHECK (status IN ('pendente', 'enviado', 'cancelado')) NOT NULL DEFAULT 'pendente',
+    numero INT, 
+	data_pedido DATE, 
+	data_entrega DATE, 
     total DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_usuario FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
 );
 
-    CREATE TABLE IF NOT EXISTS itens_pedido (
-        id SERIAL PRIMARY KEY,
-        pedido_id INT NOT NULL,
-        produto_id INT NOT NULL,
-        quantidade INT NOT NULL,
-        preco_unitario DECIMAL(10, 2) NOT NULL,
-        subtotal DECIMAL(10, 2) NOT NULL,
-        CONSTRAINT fk_pedido FOREIGN KEY (pedido_id) REFERENCES pedidos(id) ON DELETE CASCADE,
-        CONSTRAINT fk_produto FOREIGN KEY (produto_id) REFERENCES produtos(id) ON DELETE CASCADE
-    );
+CREATE TABLE IF NOT EXISTS itens_pedido (
+    id SERIAL PRIMARY KEY,
+    pedido_id INT NOT NULL,
+    produto_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    preco_unitario DECIMAL(10, 2) NOT NULL,
+    subtotal DECIMAL(10, 2) NOT NULL,
+    CONSTRAINT fk_pedido FOREIGN KEY (pedido_id) REFERENCES pedidos(id) ON DELETE CASCADE,
+    CONSTRAINT fk_produto FOREIGN KEY (produto_id) REFERENCES produtos(id) ON DELETE CASCADE
+);
